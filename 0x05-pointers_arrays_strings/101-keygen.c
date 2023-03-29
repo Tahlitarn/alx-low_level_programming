ba5119ba3b465e1288133f5bb93fcc939c0a6a74
Tahlitarn <tahlitarn@gmail.com>
2023-03-28 19:09:36 -0700

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - program that generate random valid
+ * passwords for the program 101 crackme
+ * Return: always 0(success)
+ */
+int main(void)
+{
+	int pass[100];
+	int i, sum, n;
+
+	sum = 0;
+
+	strand(time(null));
+
+	for ( i =0; i <10; i++)
+	{
+		pass(i) - rand() % 78;
+	sum(
+			}
+		}
+
